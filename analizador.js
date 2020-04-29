cf8734e8d08987b9c9ac0e7d821b963f84a080f8
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-04-28 19:33:39 -0600

    Update

--- analizador.js
+++ analizador.js
@@ -1,13 +1,33 @@
-var Parser = require("jison").Parser;
+var Parser = require('jison').Parser;
 
 let something="";
+
+var grammar = {
+    "lex": {
+        "rules": [
+           ["\\s+", "/* skip whitespace */"],
+           ["[a-f0-9]+", "return 'HEX';"]
+        ]
+    },
+
+    "bnf": {
+        "hex_strings" :[ "hex_strings HEX",
+                         "HEX" ]
+    }
+};
+
+
+var parser = new Parser(grammar);
+var parserSource = parser.generate();
+
 function parse_start(){
-    
+    parser.parse("adfe34bc e82a");
 }
 
 
 
 
 module.exports={
-    something
+    something,
+    parse_start
 };
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 app.set('port', '3000');
+let lexer=require('./analizador.js');
 app.get('/', function (req, res) {
-    
+    lexer.parse_start();
+    res.send('prus.html');
 });
+
 app.listen(app.get('port'), function () {
     console.log('Example app listening on port', app.get('port'));
+
 });
