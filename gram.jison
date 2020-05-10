8a3b25d99678784f2bcae26b13dbc69a15e94d6d
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-05-09 20:11:54 -0600

    Update

--- gram.jison
+++ gram.jison
@@ -1,4 +1,19 @@
 /* Definición Léxica */
+%{
+	//nuestras estructuras
+    let errores =[];
+    let nombres=[];
+    function in_err(tipo, lin, col, decrip){
+        var c = {errores.length, tipo, lin,col, decrip};
+        errores.push(c);
+    }
+    function in_var(tipo, nombre){
+        var c = {tipo, nombre};
+        nombres.push(c);
+    }
+%}
+
+
 %lex
 
 %options case-sensitive
@@ -16,7 +31,7 @@
 "double"                return 'DOUBLE';
 "boolean"               return 'BOOLEAN';
 "true"                  return 'TRUE';
-"false"                 return 'FALSE;
+"false"                 return 'FALSE';
 "if"                    return 'IF';
 "else"                  return 'ELSE';
 "Switch"                return 'SWITCH';
@@ -32,6 +47,7 @@
 "out"                   return 'OUT';
 "println"               return 'PRINTLN';
 "print"                 return 'PRINT';
+"continue"              return 'CONTINUE';
 "+"                     return 'MAS';
 "-"                     return 'MENOS';
 "*"                     return 'POR';
@@ -64,7 +80,99 @@
 [0-9]+\b				return 'ENTERO';
 ([a-zA-Z])[a-zA-Z0-9_]*	return 'IDENTIFICADOR';
 <<EOF>>				    return 'EOF';
-.					    { console.error('Este es un error léxico: ' + yytext + ', en la linea: ' + yylloc.first_line + ', en la columna: ' + yylloc.first_column); }
+.					    { in_err("Lexico", yylloc.first_line,yylloc.first_column, "El caracter("+yytext+")no pertenece al lenguaje"); }
 
 /lex
+%{
+	const TIPO_OPERACION	= require('./instrucciones').TIPO_OPERACION;
+	const TIPO_VALOR 		= require('./instrucciones').TIPO_VALOR;
+	const instruccionesAPI	= require('./instrucciones').instruccionesAPI;
+%}
+
+
+%start ini
+
+%% /* gramar def */
+
+ini
+	: instrucciones EOF {
+		return {$1,errores, nombre};
+	}
+;
+instrucciones
+	: instrucciones instr_main 	{ $1.push($2); $$ = $1; }
+	| instr_main				{ $$ = [$1]; }
+;
+
+
+instr_main
+	: IMPORT IDENTIFICADOR PUNTO_C 	{ $$ = instruccionesAPI.nuevoImport($2); } 
+    | CLASS IDENTIFICADOR LLAVE_A instr_methods LLAVE_C 	{ $$ = instruccionesAPI.nuevoClass($4); } 	
+	| error { console.error('Este es un error sintáctico: ' + yytext + ', en la linea: ' + this._$.first_line + ', en la columna: ' + this._$.first_column); }
+;
+
+instr_methods
+    :instr_methods instr_meth   {$1.push($2); $$ = $1; }
+    |instr_meth                 {$$ = [$1];}
+;
+instr_meth
+    : VOID IDENTIFICADOR PAR_A params PAR_C LLAVE_A instr_general LLAVE_C {$$=instruccionesAPI.nuevoMetodo($2,$4)}
+    | typo_var IDENTIFICADOR PAR_A params PAR_C LLAVE_A instr_general LLAVE_C {$$=instruccionesAPI.nuevoFuncion($2,$4,$1)}
+;
+
+
+instr_general
+    : instr_general instr	{ $1.push($2); $$ = $1; }
+    | instr                 { $$ = [$1]; }
+;
+//normal
+instr
+    : IF PAR_A declaracion PAR_C LLAVE_A instr_general LLAVE_C {$$=instruccionesAPI.nuevoIf($3,$6)}
+    | ELSE IF PAR_A declaracion PAR_C LLAVE_A instr_general LLAVE_C {$$=instruccionesAPI.nuevoElseIf($4,$7)}
+    | ELSE LLAVE_A instr_general LLAVE_C {$$=instruccionesAPI.nuevoElse($3)}
+    | WHILE PAR_A declaracion PAR_C LLAVE_A instr_general LLAVE_C {$$=instruccionesAPI.nuevoWhile($3,$6)}
+    | DO LLAVE_A instr_general LLAVE_C WHILE PAR_A declaracion PAR_C PUNTO_C {$$=instruccionesAPI.nuevoDoWhile($7,$3)}
+    | SYSTEM PUNTO OUT otro_print PAR_A asignacion PAR_C PUNTO_C  {$$=instruccionesAPI.nuevoPrint($6,$4)}
+    | IDENTIFICADOR otro_asig PUNTO_C{}
+    | FOR PAR_A var_for PUNTO_C asignacion PUNTO_C asignacion PAR_C LLAVE_A instr_general LLAVE_C {$$=instruccionesAPI.nuevoFor($3,$5,$7,$10)}
+    | typo_var IDENTIFICADOR IGUAL asignacion PUNTO_C {$$=instruccionesAPI.nuevoVal($1,$2,$4);}
+    | BREAK PUNTO_C {$$=instruccionesAPI.nuevoBreak()}
+    | RETURN asignacion PUNTO_C {$$=instruccionesAPI.nuevoReturn($2)}
+    | CONTINUE PUNTO_C {$$=instruccionesAPI.nuevoContinue()}
+;
+
+var_for
+    : typo_var IDENTIFICADOR IGUAL asignacion {$$=[$2,$4];}
+    | IDENTIFICADOR IGUAL asignacion {$$=[$1,$3]}
+;
+
+otro_asig
+    : PAR_A params2 PAR_C{}
+    | IGUAL asignacion{}
+;
+
+typo_var
+    : INT       {$$=$1}
+    | DOUBLE    {$$=$1}
+    | STRING    {$$=$1}
+    | CHAR      {$$=$1}
+    | BOOLEAN   {$$=$1}
+;
+
+otro_print
+    : PRINT {$$=$1}
+    | PRINTLN {$$=$1}
+;
+
+asignacion
+    :
+;
+
+params
+    :
+;
+
+params2
+    :
+;
 
--- instr.js
+++ instr.js
@@ -1,5 +1,267 @@
 const TIPO_VAL = {
-	NUMERO:         'V_NUMERO',
-	IDENTIFICADOR:  'V_IDENTIFICADOR',
-	CADENA:         'V_CADENA',
+	NUMERO: 'V_NUMERO',
+	IDENTIFICADOR: 'V_IDENTIFICADOR',
+	CADENA: 'V_CADENA',
+};
+
+const TIPO_OPERACION = {
+	SUMA: 'O_SUMA',
+	RESTA: 'O_RESTA',
+	MULTIPLICACION: 'O_MULTIPLICACION',
+	DIVISION: 'O_DIVISION',
+	NEGATIVO: 'O_NEGATIVO',
+	MAYOR_QUE: 'O_MAYOR_QUE',
+	MENOR_QUE: 'O_MENOR_QUE',
+	MAYOR_IGUAL: 'O_MAYOR_IGUAL',
+	MENOR_IGUAL: 'O_MENOR_IGUAL',
+	DOBLE_IGUAL: 'O_DOBLE_IGUAL',
+	NO_IGUAL: 'O_NO_IGUAL',
+	AND: 'O_AND',
+	OR: 'O_OR',
+	NOT: 'O_NOT',
+	POW: 'O_POW',
+	CONCATENACION: 'O_CONCATENACION'
+};
+
+const TIPO_INSTRUCCION = {
+	PRINT: 'INSTR_PRINT',
+	PRINTLN: 'INSTR_PRINTLN',
+	WHILE: 'INSTR_WHILE',
+	DECLARACION: 'INSTR_DECLARACION',
+	ASIGNACION: 'INSTR_ASIGANCION',
+	IF: 'INSTR_IF',
+	IF_ELSE: 'INSTR_IFELSE',
+	ELSE: 'INSTR_ELSE',
+	FOR: 'INST_FOR',
+	DO_WHILE: 'INSTR_DOWHILE',
+	SWITCH: 'SWITCH',
+	SWITCH_OP: 'SWITCH_OP',
+	SWITCH_DEF: 'SWITCH_DEF',
+	ASIGNACION_SIMPLIFICADA: 'ASIGNACION_SIMPLIFICADA',
+	IMPORT: 'IMPORT',
+	CLASS: 'CLASS',
+	MAIN: 'MAIN',
+	FUNCION: 'FUNCION',
+	CONTINUE: 'CONTINUE',
+	BREAK: 'BREAK',
+	RETURN: 'RETURN',
+	LLAMADA_F: "LLAMADA_F"
+};
+
+const TIPO_OPCION_SWITCH = {
+	CASE: 'CASE',
+	DEFAULT: 'DEFAULT'
+};
+
+function nuevaOpr(Izq, Der, tipo) {
+	return {
+		Izq: Izq,
+		Der: Der,
+		tipo: tipo
+	}
 }
+
+const instruccionesAPI = {
+
+	nuevoOpBinaria: function (Izq, Der, tipo) {
+		return nuevaOpr(Izq, Der, tipo);
+	},
+
+	nuevoImport: function (valor){
+		return {
+			tipo: TIPO_INSTRUCCION.IMPORT,
+			valor: valor
+		}
+	},
+
+	nuevoClass: function(valor){
+		return {
+			tipo: TIPO_INSTRUCCION.CLASS,
+			valor: valor
+		}
+	},
+
+	nuevoOprUnaria: function (operando, tipo) {
+		return nuevaOpr(operando, undefined, tipo);
+	},
+
+	nuevoVal: function (valor, tipo) {
+		return {
+			tipo: tipo,
+			valor: valor
+		}
+	},
+
+	nuevoPrint: function (expresionCadena,tipo) {
+		n="";
+		if(tipo=="print"){
+			n=TIPO_INSTRUCCION.PRINT;
+		}else{
+			n=TIPO_INSTRUCCION.PRINTLN;
+		}
+		return {
+			tipo: n,
+			expresionCadena: expresionCadena
+		}
+	},
+
+	nuevoWhile: function (exprLogica, instrucciones) {
+		return {
+			tipo: TIPO_INSTRUCCION.WHILE,
+			expresionLogica: exprLogica,
+			instrucciones: instrucciones
+		};
+	},
+	nuevoDoWhile: function (exprLogica, instrucciones) {
+		return {
+			tipo: TIPO_INSTRUCCION.DO_WHILE,
+			expresionLogica: exprLogica,
+			instrucciones: instrucciones
+		};
+	},
+
+	nuevoFor: function (var_arr, expresionLogica, aumento, instrucciones) {
+		var a=var_arr[0];
+		var b=var_arr[1];
+		return {
+			tipo: TIPO_INSTRUCCION.PARA,
+			expresionLogica: expresionLogica,
+			instrucciones: instrucciones,
+			aumento: aumento,
+			variable: a,
+			valorVariable: b
+		}
+	},
+
+	nuevoDeclaracion: function (identificador, tipo) {
+		return {
+			tipo: TIPO_INSTRUCCION.DECLARACION,
+			identificador: identificador,
+			tipo_dato: tipo
+		}
+	},
+	//pendiente
+	nuevoAsignacion: function (identificador, expresionNumerica) {
+		return {
+			tipo: TIPO_INSTRUCCION.ASIGNACION,
+			identificador: identificador,
+			expresionNumerica: expresionNumerica
+		}
+	},
+
+	nuevoIf: function (expresionLogica, instrucciones) {
+		return {
+			tipo: TIPO_INSTRUCCION.IF,
+			expresionLogica: expresionLogica,
+			instrucciones: instrucciones
+		}
+	},
+	nuevoElse: function (instrucciones) {
+		return {
+			tipo: TIPO_INSTRUCCION.ELSE,
+			instrucciones: instrucciones
+		}
+	},
+
+	nuevoElseIf: function (expresionLogica, instruccionesIfVerdadero, instruccionesIfFalso) {
+		return {
+			tipo: TIPO_INSTRUCCION.IF_ELSE,
+			expresionLogica: expresionLogica,
+			instruccionesIfVerdadero: instruccionesIfVerdadero,
+			instruccionesIfFalso: instruccionesIfFalso
+		}
+	},
+
+	nuevoSwitch: function (expresionNumerica, casos) {
+		return {
+			tipo: TIPO_INSTRUCCION.SWITCH,
+			expresionNumerica: expresionNumerica,
+			casos: casos
+		}
+	},
+
+	nuevoListaCasos: function (caso) {
+		var casos = [];
+		casos.push(caso);
+		return casos;
+	},
+
+	nuevoCaso: function (expresionNumerica, instrucciones) {
+		return {
+			tipo: TIPO_OPCION_SWITCH.CASO,
+			expresionNumerica: expresionNumerica,
+			instrucciones: instrucciones
+		}
+	},
+	//caso defecto
+	nuevoCasoDef: function (instrucciones) {
+		return {
+			tipo: TIPO_OPCION_SWITCH.DEFECTO,
+			instrucciones: instrucciones
+		}
+	},
+
+	nuevoOperador: function (operador) {
+		return operador
+	},
+	aignacionSimplificada: function (identificador, operador, expresionNumerica) {
+		return {
+			tipo: TIPO_INSTRUCCION.ASIGNACION_SIMPLIFICADA,
+			operador: operador,
+			expresionNumerica: expresionNumerica,
+			identificador: identificador
+		}
+	},
+	nuevoMetodo: function (nombre, params){
+		return{
+			nombre : nombre,
+			parametros: params
+		}
+	},
+	nuevoFuncion: function(nombre, params, tipo){
+		return{
+			nombre: nombre,
+			parametros:params,
+			tipo: tipo
+		}
+	},
+	nuevollamada: function (nombre, params) {
+		return{
+			tipo: TIPO_INSTRUCCION.LLAMADA_F,
+			nombre: nombre,
+			parametros:params
+		}
+	},
+	nuevoAsig: function(nombre, valores){
+		return{
+			tipo:TIPO_INSTRUCCION.ASIGNACION,
+			nombre:nombre,
+			valores:valores
+		}
+	},
+	nuevoBreak:function(){
+		//no recibe parametros
+		return{
+			tipo:TIPO_INSTRUCCION.BREAK
+		}
+	},
+	nuevoContinue:function(){
+		//no recibe parametros
+		return{
+			tipo:TIPO_INSTRUCCION.CONTINUE
+		}
+	},
+	nuevoReturn:function(valores){
+		return{
+			tipo: TIPO_INSTRUCCION.RETURN,
+			valores: valores
+		}
+	}
+
+}
+
+module.exports.TIPO_OPERACION = TIPO_OPERACION;
+module.exports.TIPO_INSTRUCCION = TIPO_INSTRUCCION;
+module.exports.TIPO_VALOR = TIPO_VALOR;
+module.exports.instruccionesAPI = instruccionesAPI;
+module.exports.TIPO_OPCION_SWITCH = TIPO_OPCION_SWITCH;
